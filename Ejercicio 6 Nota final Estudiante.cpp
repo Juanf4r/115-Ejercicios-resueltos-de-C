2de9a2ab5be9faef5fe6f65692be503367f37178
Juan Fernando Aispuro <Juanfernand2004@hotmail.com>
2023-06-05 08:56:20 -0600

    Update

--- Ejercicio 6 Nota final Estudiante.cpp
+++ Ejercicio 6 Nota final Estudiante.cpp
@@ -5,16 +5,26 @@ final.*/
 
 #include<iostream>
 #include<cstdlib>
-#include<string.h>
+#include<string>
 #include<windows.h>
 
-float n1, n2, n3, f;
+float n1, n2, n3, nf;
 std::string seguir = "S", nom;
+
+void imprimirDatos()
+{
+	std::cout << "\n\n\t             Imprimir datos y resultados             \n";
+	std::cout << "\t---------------------------------------------------------\n";
+	std::cout << "\n\tReporte de Calificaciones del estudiante: \t" << nom << "\n";
+	std::cout << "\n\tLa primera nota parcial y su porcentaje son: \t" << n1 << "\t0.25"<<"\n";
+	std::cout << "\n\tLa segunda nota parcial y su porcentaje son: \t" << n2 << "\t0.30" << "\n";
+	std::cout << "\n\tLa tercera nota parcial y su porcentaje son: \t" << n3 << "\t0.45" << "\n";
+	std::cout << "\n\tLa nota final del estudiante es: \t" << nf << "\n";
+	std::cout << "\t---------------------------------------------------------\n";
+}
 
 int main()
 {
-	
-
 	while (seguir == "S" or seguir == "s")
 	{
 		system("CLS");
@@ -22,13 +32,30 @@ int main()
 
 		std::cout << "\n\n\tPrograma que calcula la nota final del estudiante!\n";
 		std::cout << "\t---------------------------------------------------------\n";
-		std::cout << "\n\n\tDigite el apellido del estudiante\t"; std::cin >> nom;
+		std::cout << "\n\n\tDigite el apellido del estudiante\t"; std::getline(std::cin, nom);
 
 		std::cout << "\n\n\tDigite las tres notas parciales entre 0 y 5\n\t";
+		std::cin >> n1 >> n2 >> n3;
 
+		while (n1 < 0 or n1>5 or n2 < 0 or n2>5 or n3 < 0 or n3 >5)
+		{
+			std::cout << "\n\n\tError, digite las ters notas parciales entre 0 y 5\n\t";
+			std::cin >> n1 >> n2 >> n3;
+		}
+
+		nf = n1 * 0.25 + n2 * 0.3 + n3 * 0.45;
+
+		imprimirDatos();
+
+		std::cout << "\n\tDesea procesar otro estudiante? S o N\t"; std::cin >> seguir;
+		while (seguir != "S" and seguir != "s" and seguir != "n" and seguir != "N")
+		{
+			std::cout << "\n\tError. Digite S o N\t";
+			std::cin >> seguir;
+		}
 	}
 
-
+	std::cout << "\n\n\t";
 	system("pause");
 	return 0;
 }
